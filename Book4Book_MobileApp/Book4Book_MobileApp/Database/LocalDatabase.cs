326b711d92189cd85d683851abee7f078ed3d9f6
Dominik Toczek <CRPW73@motorolasolutions.com>
2020-09-25 19:40:27 +0200

    Add database implementation

---
+++ Book4Book_MobileApp/Book4Book_MobileApp/Database/LocalDatabase.cs
@@ -0,0 +1,41 @@
+﻿using Book4Book_MobileApp.Interfaces;
+using Book4Book_MobileApp.Models;
+using SQLite;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Book4Book_MobileApp.Database
+{
+    public class LocalDatabase
+    {
+        private readonly SQLiteAsyncConnection database;
+
+        public LocalDatabase(string databasePath)
+        {
+            database = new SQLiteAsyncConnection(databasePath);
+            database.CreateTableAsync<Item>().Wait();
+        }
+
+        public async Task<List<T>> GetAll<T>() where T : class, new()
+        {
+            return await database.Table<T>().ToListAsync();
+        }
+
+        public async Task<int> SaveItem<T>(T item) where T : class, ISqlite, new()
+        {
+            var result = await database.UpdateAsync(item);
+
+            if (result == 0)
+            {
+                result = await database.InsertAsync(item);
+            }
+
+            return result;
+        }
+
+        public async Task<int> DeleteItem<T>(T item) where T : class, ISqlite, new()
+        {
+            return await database.DeleteAsync(item);
+        }
+    }
+}
---
+++ Book4Book_MobileApp/Book4Book_MobileApp/Interfaces/ISqlite.cs
@@ -0,0 +1,7 @@
+﻿namespace Book4Book_MobileApp.Interfaces
+{
+    public interface ISqlite
+    {
+        int ID { get; set; }
+    }
+}
